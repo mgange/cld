79d26ce747cc7409f5892e5971f83d179e9ecc12
Geoff Young <geoff.young@hvtdc.org>
2012-08-01 12:06:42 -0400

    Made Google Analytics conditional.

--- includes/footer.php
+++ includes/footer.php
@@ -3,12 +3,18 @@
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
 
+<?php
+if(isset($config['GAcode']) && $config['GAcode'] !== '') {
+?>
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
     <script>
-        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        var _gaq=[['_setAccount','<?php echo $config['GAcode']; ?>'],['_trackPageview']];
         (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
         g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
         s.parentNode.insertBefore(g,s)}(document,'script'));
     </script>
+<?php
+}
+?>
 </body>
 </html>
