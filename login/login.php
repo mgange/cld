5ec0dce530c25315baf527e7ee62f91d522a945e
Geoff Young <geoff.young@hvtdc.org>
2012-08-02 13:22:00 -0400

    Using the dbQuery function instead of the manual query mess.

--- login/login.php
+++ login/login.php
@@ -5,12 +5,6 @@ require_once('../general/util.php');
 
 if(isset($_POST['submit'])){
     try{
-    /* Connect by creating a ew database object. */
-        $DBH = new PDO(
-            "mysql:host=" . $config['dbHost'] . ";dbname=" . $config['dbName'],
-            $config['dbUser'],
-            $config['dbPass']
-        );
         /* Bind values to be used in the prepared statement. */
         $bind = array(
             ":username" => $_POST['username'],
@@ -18,9 +12,7 @@ if(isset($_POST['submit'])){
         );
         /* Execute the query and see if any results come back. */
         $query = "SELECT * FROM users WHERE username = :username AND password = :password";
-        $STH = $DBH->prepare($query);
-        $STH->execute($bind);
-        $results = $STH->fetch(PDO::FETCH_ASSOC);
+        $results = dbQuery($config, $query, $bind);
 
         if(count($results) > 0) {
             $_SESSION['userID']     = $results['userID'];
