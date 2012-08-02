1437957b0332e2b21f250c14a496ef8f53c39c6f
Geoff Young <geoff.young@hvtdc.org>
2012-08-02 13:20:35 -0400

    Added a database query function
    Must be passed the general config array, a query string,
    and an optional array of bound values for use in prepared
    statements.

--- general/util.php
+++ general/util.php
@@ -8,4 +8,17 @@ function hashPassword($pass)
     return sha1($pass);
 }
 
+function dbQuery($query, $bind = array(), $config)
+{
+    $DBH = new PDO(
+        "mysql:host=" . $config['dbHost'] . ";dbname=" . $config['dbName'] ,
+        $config['dbUser'],
+        $config['dbPass']
+    );
+
+    $STH = $DBH->prepare($query);
+    $STH->execute($bind);
+    return $STH->fetch(PDO::FETCH_ASSOC);
+}
+
 ?>
