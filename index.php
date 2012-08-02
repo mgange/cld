9bb6d6620329afc45e97c50c9dea273ee85e7b9c
Geoff Young <geoff.young@hvtdc.org>
2012-08-02 10:51:11 -0400

    Changed how the sites location is stored in a session.

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 session_start();
-if(! include('config/config.php')) {
-    die('No config file could not be loaded.');
+if(! require_once('config/config.php')) {
+    die('Config file could not be loaded by index.');
 }
-$_SESSION['base_url'] = $config['base_url'];
+$_SESSION['base_domain'] = $config['base_domain'];
+$_SESSION['base_dir'] = $config['base_dir'];
+
 require_once('includes/header.php');
 
+if(! isset($_SESSION['userID'])) {
+    include_once('includes/noLoginHome.php');
+}else{
+    include_once('includes/loginHome.php');
+}
 
 require_once('includes/footer.php');
 ?>
