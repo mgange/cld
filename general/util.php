eb2b58a7672767e6dc5ce1f8c41ee19408c4b46a
Geoff Young <geoff.young@hvtdc.org>
2012-08-03 11:28:42 -0400

    Created a database class to wrap PDO
    This should simplify executing queries, but is still limited for
    the moment. Only fetches one row or many rows.

--- general/util.php
+++ general/util.php
@@ -8,17 +8,87 @@ function hashPassword($pass)
     return sha1($pass);
 }
 
-function dbQuery($config, $query, $bind = array())
-{
-    $DBH = new PDO(
-        "mysql:host=" . $config['dbHost'] . ";dbname=" . $config['dbName'] ,
-        $config['dbUser'],
-        $config['dbPass']
-    );
 
-    $STH = $DBH->prepare($query);
-    $STH->execute($bind);
-    return $STH->fetch(PDO::FETCH_ASSOC);
+class db extends PDO
+{
+
+    public function __construct($config)
+    {
+        parent::__construct(
+            "mysql:host=".$config['dbHost'].";dbname=".$config['dbName'], 
+            $config['dbUser'], 
+            $config['dbPass']
+        );
+
+        try
+        {
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e)
+        {
+            die($e->getMessage());
+        }
+    }
+
+    public function fetchRow($query, $bind = array())
+    {
+        # create a prepared statement
+        $sth = parent::prepare($query);
+
+        if($sth) 
+        {
+            # execute query 
+            $sth->execute($bind);
+
+            return $sth->fetch(PDO::FETCH_ASSOC);
+        } 
+        else
+        {
+            return self::error_info();
+        }
+    }
+
+    public function fetchAll($query, $bind = array())
+    {
+        $sth = parent::prepare($query);
+
+        if($sth)
+        {
+            $sth->execute($bind);
+
+            return $sth->fetchALL(PDO::FETCH_ASSOC);
+        } 
+        else
+        {
+            return self::error_info();
+        }
+    }
+
+    public function numRows($query, $bind = array())
+    {
+        $sth = parent::prepare($query);
+
+        if($sth) {
+            # execute query 
+            $sth->execute($bind);
+
+            return $sth->rowCount();
+        } 
+        else
+        {
+            return self::error_info();
+        }
+    }
+
+    public function errorInfo() 
+    {
+        $this->connection->errorInfo();
+    }
+
+    public function __destruct()
+    {
+        $this->connection = null;
+    }
 }
 
 ?>
