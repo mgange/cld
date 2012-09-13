f6b49384eb3cb4e8a6a2bf8df11a5b56b84f7bb3
Geoff Young <geoff.young@hvtdc.org>
2012-09-13 16:53:53 -0400

    Added HVTDC.org link

--- includes/footer.php
+++ includes/footer.php
@@ -6,16 +6,26 @@
  *
  * This file should be included at the beginning of every page that is displayed
  * to the user. This will close the .container div opened in includes/header.php
- * so the opening and closing tags in the files that include header.php and 
+ * so the opening and closing tags in the files that include header.php and
  * footer.php should be self contained.
  *
- * This page sets up links to javascript files, conditional Google Ananlytics 
+ * This page sets up links to javascript files, conditional Google Ananlytics
  * script (based on the global config file), and closes the <body> tag.
  *
  * PHP 5.3.0
  */
 ?>
     </div><!-- End of .container -->
+    <footer>
+        <br>
+        <div class="container">
+            <small>
+                Lovingly handcrafted at
+                <a href="http://hvtdc.org" title="HVTDC">HVTDC</a>
+            </small>
+        </div>
+        <br>
+    </footer>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
     <script src="<?php echo $_SESSION['base_domain'] . $_SESSION['base_dir']; ?>js/plugins.js"></script>
