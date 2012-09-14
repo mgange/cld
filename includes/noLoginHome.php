91911e8e75a29423ce86fc8a4f5e8807ff9b1f45
Geoff Young <geoff.young@hvtdc.org>
2012-09-14 15:16:47 -0400

    Added some placeholder content.

--- includes/noLoginHome.php
+++ includes/noLoginHome.php
@@ -4,19 +4,96 @@
  * Homepage File for Non-Logged In Users
  *------------------------------------------------------------------------------
  *
- * This page is displayed at the root of the sites structure but NOT to logged 
- * in users. It is a placeholder that will be replaced by site specific 
+ * This page is displayed at the root of the sites structure but NOT to logged
+ * in users. It is a placeholder that will be replaced by site specific
  * information such as news, a blog, or marketing information for the site.
  *
  */
 ?>
-<h1>Welcome</h1>
-<h3>This message is for people who haven't logged in yet.</h3>
-<p>Try signing in with the username <strong>demo</strong> and the password <strong>demo</strong>.</p>
-<p>also ...</p>
+<div class="hero-unit">
+    <h1>Welcome</h1>
+    <p>Try signing with one of the demo user accounts.</p>
+</div>
 <pre>
     user    : user
     manager : manager
     admin   : admin
 </pre>
+<div class="row">
+    <div class="span6">
+        <h2>Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+            Pellentesque convallis tortor quis orci porttitor vel dignissim
+            lorem feugiat. Cras vel arcu mi, ut interdum purus.</p>
+    </div>
+    <div class="span6">
+        <h2>Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+            Pellentesque convallis tortor quis orci porttitor vel dignissim
+            lorem feugiat. Cras vel arcu mi, ut interdum purus.</p>
+    </div>
+</div>
 
+<hr>
+
+<div class="row">
+    <div class="span4">
+        <h4>Section</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+        </p>
+
+    </div>
+    <div class="span4">
+        <h4>Section</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+        </p>
+    </div>
+    <div class="span4">
+        <h4>Section</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+        </p>
+    </div>
+</div>
+
+<hr>
+
+<div class="row">
+    <div class="span3">
+        <h4>Details</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+        </p>
+    </div>
+    <div class="span3">
+        <h4>Details</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+        </p>
+    </div>
+    <div class="span3">
+        <h4>Details</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+        </p>
+    </div>
+    <div class="span3">
+        <h4>Details</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
+            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
+            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
+        </p>
+    </div>
+</div>
