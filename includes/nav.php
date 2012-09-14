e66c92160af46ef97e88649fe19649134c88462c
Geoff Young <geoff.young@hvtdc.org>
2012-09-14 15:24:15 -0400

    Used real name when available instead of username.

--- includes/nav.php
+++ includes/nav.php
@@ -4,8 +4,8 @@
  * Global Navigation File
  *------------------------------------------------------------------------------
  *
- * This page is imported into the navbar when a user is logged in. It should 
- * include site specific links and can build them out based on the users 
+ * This page is imported into the navbar when a user is logged in. It should
+ * include site specific links and can build them out based on the users
  * information in the $_SESSION array or site data in the global $config array.
  * The markup is indented enough to align it with the navbar markup when a page
  * is generated( if that matters to you).
@@ -14,8 +14,13 @@
 ?>
             <div class="btn-group pull-right">
                 <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-                    <i class="icon-user"></i> <?php echo $_SESSION['username'] ?>
-                    <span class="caret"></span>
+                    <i class="icon-user"></i> <?php
+if($_SESSION['firstName'] != '' && $_SESSION['lastName'] != '') {
+    echo $_SESSION['firstName'] . ' ' . $_SESSION['lastName'];
+}else{
+    echo $_SESSION['username'];
+}
+                     ?> <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu">
                     <li><a href="<?php echo $config['base_domain'] . $config['base_dir']; ?>profile">Profile</a></li>
