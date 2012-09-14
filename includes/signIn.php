1a940bb1959b2b46dcdd6f041dcd474501d51dec
Geoff Young <geoff.young@hvtdc.org>
2012-09-14 15:14:30 -0400

    Added labels to the sign in form.
    no placeholders, probably IE's fault.

--- includes/signIn.php
+++ includes/signIn.php
@@ -4,18 +4,22 @@
  * Sign In Form
  *------------------------------------------------------------------------------
  *
- * This page is imported into the navbar when a user is NOT logged in. The 
- * markup is indented enough to align it with the navbar markup when a page is 
+ * This page is imported into the navbar when a user is NOT logged in. The
+ * markup is indented enough to align it with the navbar markup when a page is
  * generated( if that matters to you).
  *
  */
 ?>
-            <form 
-            class="sign-in-form pull-right form-inline" 
-            action="<?php echo $_SESSION['base_url']; ?>login/login.php" 
+            <form
+            class="sign-in-form pull-right form-inline"
+            action="<?php echo $_SESSION['base_url']; ?>login/login.php"
             method="POST">
-                <input type="text" name="username" placeholder="Username" >
-                <input type="password" name="password" placeholder="Password" >
+                <label for="username">Username &nbsp;
+                    <input id="username" type="text" name="username" >
+                </label>
+                <label for="password">Password &nbsp;
+                    <input id="password" type="password" name="password" >
+                </label>
 
                 <input class="submit btn btn-small" type="submit" name="submit" value="Sign In">
             </form>
