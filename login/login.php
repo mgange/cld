fc8e4495cdef9fbe382d2f9a7ee6bd23bfe280c0
Geoff Young <geoffreysyoung@gmail.com>
2012-09-23 20:45:40 -0400

    Removed an unnecessary try/catch

--- login/login.php
+++ login/login.php
@@ -15,32 +15,28 @@ require_once('../config/config.php');
 require_once('../general/util.php');
 
 if(isset($_POST['submit'])){
-    try{
-        /* Bind values to be used in the prepared statement. */
-        $bind = array(
-            ":username" => $_POST['username'],
-            ":password" => hashPassword($_POST['password'])
-        );
-        /* Execute the query and see if any results come back. */
-        $query = "SELECT * FROM users WHERE username = :username AND password = :password";
-        $db = new db($config);
-        $results = $db -> fetchRow($query, $bind);
+    /* Bind values to be used in the prepared statement. */
+    $bind = array(
+        ":username" => $_POST['username'],
+        ":password" => hashPassword($config, $_POST['password'])
+    );
+    /* Execute the query and see if any results come back. */
+    $query = "SELECT * FROM users WHERE username = :username AND password = :password";
+    $db = new db($config);
+    $results = $db -> fetchRow($query, $bind);
 
-        if(count($results) > 0) {
-            $_SESSION['userID']     = $results['userID'];
-            $_SESSION['customerID'] = $results['customerID'];
-            $_SESSION['username']   = $results['username'];
-            $_SESSION['authLevel']  = $results['authLevel'];
-            $_SESSION['email']      = $results['email'];
-            $_SESSION['firstName']  = $results['firstName'];
-            $_SESSION['lastName']   = $results['lastName'];
-            $_SESSION['last_activity'] = time();
-            $_SESSION['last_update']   = time();
-        }
-
-    } catch (Exception $e) {
-        echo $e->getMessage();
+    if(count($results) > 0) {
+        $_SESSION['userID']     = $results['userID'];
+        $_SESSION['customerID'] = $results['customerID'];
+        $_SESSION['username']   = $results['username'];
+        $_SESSION['authLevel']  = $results['authLevel'];
+        $_SESSION['email']      = $results['email'];
+        $_SESSION['firstName']  = $results['firstName'];
+        $_SESSION['lastName']   = $results['lastName'];
+        $_SESSION['last_activity'] = time();
+        $_SESSION['last_update']   = time();
     }
+
 
     header('Location: ../');
 }
