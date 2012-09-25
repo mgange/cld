2cf5c8be49c96f8956ed1e17e2ece3fbd6091967
Geoff Young <geoff.young@hvtdc.org>
2012-09-25 16:49:38 -0400

    Fixed indentation on admin index page

--- admin/index.php
+++ admin/index.php
@@ -48,97 +48,96 @@ $users = $db -> fetchAll($query);
 
 foreach($customers as $cust) {
 ?>
-<div class="accordion-group">
-    <div class="accordion-heading">
-        <a class="accordion-toggle"
-            data-toggle="collapse"
-            data-parent="#accordion2"
-            href="#collapse<? echo $cust['customerID']; ?>">
-        <h3><?php echo $cust['customerName']; ?></h3>
-        </a>
-    </div>
-    <div id="collapse<?php echo $cust['customerID']; ?>"
-    class="accordion-body collapse<?php if(count($customers) == 1){echo ' in';} ?>">
-        <div class="accordion-inner">
-            <div class="row">
-                <div class="span5">
-                    <p><?php echo $cust['addr1']; ?></p>
-                    <p><?php echo $cust['addr2']; ?></p>
-                    <p><?php echo $cust['city'].', '.$cust['state'].' '.$cust['zip']; ?></p>
-                </div>
-                <div class="span5">
-                    <p><a href="mailto:<?php echo $cust['email1']; ?>"><?php echo $cust['email1']; ?></a></p>
-                    <p><a href="mailto:<?php echo $cust['email2']; ?>"><?php echo $cust['email2']; ?></a></p>
-                    <a href="customer?id=<?php echo $cust['customerID']; ?>" class="btn">
-                    <i class="icon-edit"></i>
-                    Edit Customer Info
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <a class="accordion-toggle"
+                    data-toggle="collapse"
+                    data-parent="#accordion2"
+                    href="#collapse<? echo $cust['customerID']; ?>">
+                <h3><?php echo $cust['customerName']; ?></h3>
                 </a>
-                </div>
             </div>
+            <div id="collapse<?php echo $cust['customerID']; ?>" class="accordion-body collapse<?php if(count($customers) == 1){echo ' in';} ?>">
+                <div class="accordion-inner">
+                    <div class="row">
+                        <div class="span5">
+                            <p><?php echo $cust['addr1']; ?></p>
+                            <p><?php echo $cust['addr2']; ?></p>
+                            <p><?php echo $cust['city'].', '.$cust['state'].' '.$cust['zip']; ?></p>
+                        </div>
+                        <div class="span5">
+                            <p><a href="mailto:<?php echo $cust['email1']; ?>"><?php echo $cust['email1']; ?></a></p>
+                            <p><a href="mailto:<?php echo $cust['email2']; ?>"><?php echo $cust['email2']; ?></a></p>
+                            <a href="customer?id=<?php echo $cust['customerID']; ?>" class="btn">
+                            <i class="icon-edit"></i>
+                            Edit Customer Info
+                        </a>
+                        </div>
+                    </div>
 
-            <br><br>
+                    <br><br>
 
-            <h4 class="pull-left">User Accounts</h4>
+                    <h4 class="pull-left">User Accounts</h4>
 <?php
 if($_SESSION['authLevel'] == 3) {
 ?>
-<a href="new/user?id=<?php echo $cust['customerID']; ?>" class="btn btn-small btn-success offset1">
-    <i class="icon-plus icon-white"></i>
-    Add User
-</a>
+                    <a href="new/user?id=<?php echo $cust['customerID']; ?>" class="btn btn-small btn-success offset1">
+                        <i class="icon-plus icon-white"></i>
+                        Add User
+                    </a>
 <?php
 }
 ?>
-            <div class="clearfix"></div>
+                    <div class="clearfix"></div>
 <?php
             foreach($users as $user) {
                 if($user['customerID'] == $cust['customerID']) {
                     if($user['customerID'] == $cust['customerID']) {
 ?>
-            <div class="row">
-                <div class="span12">
-                    <a href="user?id=<?php echo $user['userID']; ?>">
-                        <?php
+                    <div class="row">
+                        <div class="span12">
+                            <a href="user?id=<?php echo $user['userID']; ?>">
+                                <?php
                             echo $user['firstName'].' '.$user['lastName'];
-                        ?>
+                            ?>
 
-                    </a>
+                            </a>
 <?php
 if($user['authLevel'] == 2) {
 ?>
-                    <span class="label label-info">Manager</span><?php
+                            <span class="label label-info">Manager</span><?php
 }
 if($user['authLevel'] == 3) {
 ?>
-                    <span class="label label-important">Site Admin</span><?php
+                            <span class="label label-important">Site Admin</span><?php
 }
 ?>
 
-                </div>
-            </div>
+                        </div>
+                    </div>
 <?php
                     }
                 }
             }
             ?>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
 <?php
 }
 
 if($_SESSION['authLevel'] == 3) {
 ?>
 
-<div class="row">
-    <br>
-    <div class="span12">
-        <a href="new/customer" class="btn btn-success pull-right">
-            <i class="icon-plus icon-white"></i>
-            Add New Customer
-        </a>
-    </div>
-</div>
+        <div class="row">
+            <br>
+            <div class="span12">
+                <a href="new/customer" class="btn btn-success pull-right">
+                    <i class="icon-plus icon-white"></i>
+                    Add New Customer
+                </a>
+            </div>
+        </div>
 <?php
 }
 
