22e5a0149f2fdbf8dfd4dae38278bf6a496d7ed5
Geoff Young <geoff.young@hvtdc.org>
2012-09-28 16:31:17 -0400

    Assorted cleanup
    Seperated PHP from markup
    Removed <font> and <center> tags in favor of classes and css
    Indented to match header and footer.

--- includes/loginHome.php
+++ includes/loginHome.php
@@ -11,100 +11,41 @@
  *
  */
 ?>
-<div class="hero-unit" ><h2>
-<?php
-   {
-   echo 'CLD is a Industry Leader in Alternative Energy Systems. Their GeoThermal';
-   echo ' Monitoring System provides home owners and building managers with real system status and';
-   echo ' system effiency monitors and long term insight into their energy savings realizations.';
-   }
-?>
-    </h2></div>
-<div class="hero-unit" ><h3><center>
+        <div class="hero-unit" >
+            <h2>CLD is a Industry Leader in Alternative Energy Systems. Their GeoThermal
+                Monitoring System provides home owners and building managers with real
+                system status and system effiency monitors and long term insight into
+                their energy savings realizations.
+            </h2>
+        </div>
+        <div class="hero-unit" >
+            <h3 class="align-center">
 <?php
 if( isset($_SESSION['userID'])){
-   echo '<a href="dashboard/index.php" >GeoThermal System Monitor</a>';
-   }
-else {  echo '<font color="blue">';
-        echo 'Please Sign In above to Monitor your Systems';
-        echo '<font color="black">';
-     }
 ?>
-     </center> </h3></div>
-<!--<pre>
-   user    : user
-    manager : manager
-    admin   : admin
-</pre> -->
-<div class="row">
-    <div class="span6">
-        <h3>Learn More about GeoThermal Systems</h2>
-        <p><a href="http://energy.gov/energysaver/articles/geothermal-heat-pumps">
-                Energy.Gov - GeoThermal Heat Pumps"</a></p></h3>
-    </div>
-   <div class="span6">
-        <h3>Home Geothermal System</h3>
-        <p><img src="img/geothermal-heat-pump-systems.jpg" width="414" height="342" longdesc="geothermal-heat-pump-systems.jpg" /></p>
-   </div>
-</div>
-<!--
-<hr>
+                <a href="dashboard/index.php" >GeoThermal System Monitor</a>
+<?php
+}else{
+?>
+                    Please Sign In above to Monitor your Systems
+<?php
+}
+?>
+            </h3>
+        </div>
 
-<div class="row">
-    <div class="span4">
-        <h4>Section</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
-            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
-            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
-        </p>
-
-    </div>
-    <div class="span4">
-        <h4>Section</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
-            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
-            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
-        </p>
-    </div>
-    <div class="span4">
-        <h4>Section</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
-            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
-            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
-        </p>
-    </div>
-</div>
-
-<hr>
-
-<div class="row">
-    <div class="span3">
-        <h4>Details</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
-            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
-            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
-        </p>
-    </div>
-    <div class="span3">
-        <h4>Details</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
-            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
-            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
-        </p>
-    </div>
-    <div class="span3">
-        <h4>Details</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
-            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
-            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
-        </p>
-    </div>
-    <div class="span3">
-        <h4>Details</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue
-            laoreet fringilla. Vivamus sed arcu sed libero pulvinar facilisis at
-            id felis. Nullam at felis at magna malesuada adipiscing ut quis mi.
-        </p>
-    </div>
-</div>
--->
+        <div class="row">
+            <div class="span6">
+                <h3>Learn More about GeoThermal Systems</h2>
+                <p>
+                    <a href="http://energy.gov/energysaver/articles/geothermal-heat-pumps">
+                        Energy.Gov - GeoThermal Heat Pumps"
+                    </a>
+                </p>
+            </h3>
+            </div>
+           <div class="span6">
+                <h3>Home Geothermal System</h3>
+                <p><img src="img/geothermal-heat-pump-systems.jpg" width="414" height="342" longdesc="geothermal-heat-pump-systems.jpg" /></p>
+           </div>
+        </div>
