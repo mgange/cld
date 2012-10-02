8f44dabde2ab8ea1a237ca7df0059b515ddfe99e
Geoff Young <geoff.young@hvtdc.org>
2012-10-02 13:41:53 -0400

    Added more filtering for user access
    Only check buildings if system isn't set in SESSION
    If it is set then confirm that it belongs to the
    user in question

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,51 +7,76 @@
  */
 require_once('../includes/header.php');
 
+systemSwitch($config);
+
 $db = new db($config);
 
-// Get the building info for this user's customer account
-$query = 'SELECT buildingID FROM buildings WHERE customerID = customerID';
-$buildingsBind[':customerID'] = $_SESSION['customerID'];
+if(!isset($_SESSION['buildingID']) || !isset($_SESSION['SysID'])) {
 
-$buildings = $db -> fetchAll($query, $buildingsBind);
+    // Get the building info for this user's customer account
+    $query = 'SELECT * FROM buildings WHERE customerID = :customerID';
+    $buildingsBind[':customerID'] = $_SESSION['customerID'];
 
-/**
- * If they have more than one building send them to a page where they can choose
- * what they want a dashboard for. Otherwise set buildingID as a session
- * variable.
- */
-switch(count($buildings)) {
-    case 0:
+    $buildings = $db -> fetchAll($query, $buildingsBind);
+    pprint($buildings);
+    /**
+     * If they have more than one building send them to a page where they can choose
+     * what they want a dashboard for. Otherwise set buildingID as a session
+     * variable.
+     */
+    switch(count($buildings)) {
+        case 0:
+            gtfo($config);
+            break;
+        case 1:
+            $_SESSION['buildingID'] = $buildings[0]['buildingID'];
+            break;
+        default:
+            header('Location: buildings');
+            break;
+    }
+
+    /**
+     * If they have more than one system send them to a page where they can choose
+     * what they want a dashboard for. Otherwise set sysID as a session
+     * variable.
+     */
+    $query = 'SELECT sysID FROM SystemConfig WHERE buildingID = :buildingID';
+    $systemConfigBind[':buildingID'] = $buildings[0]['buildingID'];
+
+    $sysConfigs = $db -> fetchAll($query, $systemConfigBind);
+
+    switch(count($sysConfigs)) {
+        case 0:
+            gtfo($config);
+            break;
+        case 1:
+            $_SESSION['SysID'] = $sysConfigs[0]['SysID'];
+            break;
+        default:
+            header('Location: buildings');
+            break;
+    }
+
+}else{
+    // Double check that the building/system belongs to their customer account
+    $buildingResponse = $db -> numRows(
+        'SELECT customerID FROM buildings WHERE buildingID = :buildingID',
+        array(':buildingID' => intval($_SESSION['buildingID']))
+        );
+    $sysConfigResponse = $db -> numRows(
+        'SELECT buildings.customerID
+        FROM buildings LEFT JOIN SystemConfig
+        ON buildings.buildingID = SystemConfig.buildingID
+        WHERE SystemConfig.SysID = :SysID',
+        array(':SysID' => intval($_SESSION['SysID']))
+        );
+    if(!$buildingResponse || !$sysConfigResponse) {
         gtfo($config);
-        break;
-    case 1:
-        $_SESSION['buildingID'] = $buildings[0]['buildingID'];
-        break;
-    default:
-        header('Location: buildings');
-        break;
-}
+    }else{
+        // Content goes here
+    }
 
-/**
- * If they have more than one system send them to a page where they can choose
- * what they want a dashboard for. Otherwise set sysID as a session
- * variable.
- */
-$query = 'SELECT sysID FROM SystemConfig WHERE buildingID = :buildingID';
-$systemConfigBind[':buildingID'] = $buildings[0]['buildingID'];
-
-$sysConfigs = $db -> fetchAll($query, $systemConfigBind);
-
-switch(count($sysConfigs)) {
-    case 0:
-        gtfo($config);
-        break;
-    case 1:
-        $_SESSION['sysID'] = $sysConfigs[0]['sysID'];
-        break;
-    default:
-        header('Location: buildings');
-        break;
 }
 
 ?>
