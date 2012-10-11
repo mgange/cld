544fc063d557ff0cc7e4a61470ed9e5f24bc7d68
Geoff Young <geoff.young@hvtdc.org>
2012-10-11 15:09:11 -0400

    Added titles

--- performance/index.php
+++ performance/index.php
@@ -6,6 +6,12 @@
  *
  */
 require_once('../includes/header.php');
+?>
 
+        <div class="row">
+            <h1 class="span8 offset2">Performance</h1>
+        </div>
+
+<?php
 require_once('../includes/footer.php');
 ?>
