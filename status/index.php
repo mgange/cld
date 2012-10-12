c02092f6fbeef39cbfb2931c5d569044cbe2db57
Geoff Young <geoff.young@hvtdc.org>
2012-10-12 09:55:02 -0400

    Set up sample layered images.
    The status map will be build similarly

--- status/index.php
+++ status/index.php
@@ -12,6 +12,17 @@ require_once('../includes/header.php');
             <h1 class="span8 offset2">Status</h1>
         </div>
 
+        <div class="row">
+            <div class="status-container span8 offset2">
+                <div class="status-child map">
+                    <img src="../img/SysIll.png" alt="alt">
+                </div>
+                <div class="status-child glyphs">
+                    <img src="../img/glyphicons-halflings.png" alt="alt">
+                </div>
+            </div>
+        </div>
+
 <?php
 require_once('../includes/footer.php');
 ?>
