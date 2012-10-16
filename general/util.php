3c7d9cdc88c41c5d83e8e215a6a74cb9707b1b5a
Geoff Young <geoff.young@hvtdc.org>
2012-10-15 20:26:54 -0400

    Re-wrote session system check.
    now checking if the customer has only one system, and if so
    set that system in the session array. Otherwise send them to
    the system picker.

--- general/util.php
+++ general/util.php
@@ -55,11 +55,76 @@ function gtfo($config){
  */
 function checkSystemSet($config)
 {
-    if(!isset($_SESSION['buildingID']) || !isset($_SESSION['SysId'])) {
-        header('Location: '
-            . $config['base_domain']
-            . $config['base_dir']
-            . 'systems');
+    $db = new db($config);
+
+    if(!isset($_SESSION['buildingID']) || !isset($_SESSION['SysID'])) {
+
+        // Get the building info for this user's customer account
+        $buildingQuery = 'SELECT * FROM buildings WHERE customerID = :customerID';
+        $buildingsBind[':customerID'] = $_SESSION['customerID'];
+
+        $buildings = $db -> fetchAll($buildingQuery, $buildingsBind);
+        /**
+         * If they have more than one building send them to a page where they can choose
+         * what they want a dashboard for. Otherwise set buildingID as a session
+         * variable.
+         */
+        switch(count($buildings)) {
+            case 0:
+                gtfo($config);
+                break;
+            case 1:
+                $_SESSION['buildingID'] = $buildings[0]['buildingID'];
+                break;
+            default:
+                header('Location: '
+                    . $config['base_domain']
+                    . $config['base_dir']
+                    . 'systems');
+                break;
+        }
+
+        /**
+         * If they have more than one system send them to a page where they can choose
+         * what they want a dashboard for. Otherwise set sysID as a session
+         * variable.
+         */
+        $systemQuery = 'SELECT sysID FROM SystemConfig WHERE buildingID = :buildingID';
+        $systemConfigBind[':buildingID'] = $buildings[0]['buildingID'];
+
+        $sysConfigs = $db -> fetchAll($systemQuery, $systemConfigBind);
+
+        switch(count($sysConfigs)) {
+            case 0:
+                gtfo($config);
+                break;
+            case 1:
+                $_SESSION['SysID'] = $sysConfigs[0]['SysID'];
+                break;
+            default:
+                header('Location: '
+                    . $config['base_domain']
+                    . $config['base_dir']
+                    . 'systems');
+                break;
+        }
+
+    }else{
+        // Double check that the building/system belongs to their customer account
+        $buildingResponse = $db -> numRows(
+            'SELECT customerID FROM buildings WHERE buildingID = :buildingID',
+            array(':buildingID' => intval($_SESSION['buildingID']))
+            );
+        $sysConfigResponse = $db -> numRows(
+            'SELECT buildings.customerID
+            FROM buildings LEFT JOIN SystemConfig
+            ON buildings.buildingID = SystemConfig.buildingID
+            WHERE SystemConfig.SysID = :SysID',
+            array(':SysID' => intval($_SESSION['SysID']))
+            );
+        if(!$buildingResponse || !$sysConfigResponse) {
+            gtfo($config);
+        }
     }
 }
 
