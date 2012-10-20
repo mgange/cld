05967458104be406ebb41426b3e374c3f5982fc6
Bob Incerto <bob.incerto@gmail.com>
2012-10-19 20:00:35 -0400

    Added Contact us content

--- contact/index.php
+++ contact/index.php
@@ -11,6 +11,10 @@ require_once('../includes/header.php');
 
         <div class="row">
             <h1 class="span8 offset2">Contact Us</h1>
+            <h3 class="span10 ">
+            CLD Industries LLC <br> 315 East Kerley Corners Road, <br> Tivoli, NY 12583
+            <br> Denis Collet Principle Owner
+            <br> Email:dcollet@frontiernet.net
         </div>
 
 <?php
