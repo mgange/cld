802dcdb8fa30a241dc2598a7eb587495ba81a52c
James Lynch <james.lynch@hvtdc.org>
2012-10-24 10:08:33 -0400

    Switched php echos to shorttags

--- information/index.php
+++ information/index.php
@@ -22,7 +22,7 @@ require_once('../includes/header.php');
         $customerInfo = $db -> fetchRow($query);
 ?>
         <div class="row">
-            <h1 class="span8 offset2">'<?php echo $systemInfo['SysName']; ?>' Information</h1>
+            <h1 class="span8 offset2">'<?=$systemInfo['SysName']?>' Information</h1>
         </div>
 
         <div class="row">
@@ -32,14 +32,14 @@ require_once('../includes/header.php');
 
                 <div class="row">
                     <p class="span3"><strong>Customer Name:</strong></p>
-                    <p class="span3"><?php echo $customerInfo['customerName']; ?></p>
+                    <p class="span3"><?=$customerInfo['customerName']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Customer Location:</strong></p>
-                    <p class="span3"><?php echo $customerInfo['addr1'] . "<br>" . (isset($customerInfo['addr2']) ? $customerInfo['addr2'] . "<br>" : "") . $customerInfo['city'] . ", " . $customerInfo['state'] . " " . $customerInfo['zip']; ?></p>
+                    <p class="span3"><?=$customerInfo['addr1'] . "<br>" . (!empty($customerInfo['addr2']) ? $customerInfo['addr2'] . "<br>" : "") . $customerInfo['city'] . ", " . $customerInfo['state'] . " " . $customerInfo['zip']?></p>
                 </div>
 
                 <hr>
@@ -60,28 +60,28 @@ require_once('../includes/header.php');
 
                 <div class="row">
                     <p class="span3"><strong>Building Location:</strong></p>
-                    <p class="span3"><?php echo $buildingInfo['address1'] . "<br>" . (isset($buildingInfo['address2']) ? $buildingInfo['address2'] . "<br>" : "") . $buildingInfo['city'] . ", " . $buildingInfo['state'] . " " . $buildingInfo['zip']; ?></p>
+                    <p class="span3"><?=$buildingInfo['address1'] . "<br>" . (!empty($buildingInfo['address2']) ? $buildingInfo['address2'] . "<br>" : "") . $buildingInfo['city'] . ", " . $buildingInfo['state'] . " " . $buildingInfo['zip']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Install Date:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['InstallDate']; ?></p>
+                    <p class="span3"><?=$systemInfo['InstallDate']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Installer:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['Installer']; ?></p>
+                    <p class="span3"><?=$systemInfo['Installer']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Maintainer:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['Maintainer']; ?></p>
+                    <p class="span3"><?=$systemInfo['Maintainer']?></p>
                 </div>
             </div>
 
@@ -91,70 +91,82 @@ require_once('../includes/header.php');
 
                 <div class="row">
                     <p class="span3"><strong>DAMID:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['DAMID']; ?></p>
+                    <p class="span3"><?=$systemInfo['DAMID']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>System Type:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['Systype']; ?></p>
+                    <p class="span3"><?=$systemInfo['Systype']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Configuration:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['Configuration']; ?></p>
+                    <p class="span3"><?php
+                        switch ($systemInfo['Configuration']){
+                            case 1:
+                                echo "Open Loop System with Dry Well";
+                                break;
+                            case 2:
+                                echo "Open Loop System without Dry Well";
+                                break;
+                            case 3:
+                                echo "Closed Loop System";
+                                break;
+                        }
+                    ?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Heat Exchange Unit:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['HeatExchangeUnit']; ?></p>
+                    <p class="span3"><?=$systemInfo['HeatExchangeUnit']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Number of Sensor Group:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['NumofSensGrp']; ?></p>
+                    <p class="span3"><?=$systemInfo['NumofSensGrp']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Number of Digital Sensor Channels:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['NumDigSenChan']; ?></p>
+                    <p class="span3"><?=$systemInfo['NumDigSenChan']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Number of Temperature Sensor Channels:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['NumTempSenChan']; ?></p>
+                    <p class="span3"><?=$systemInfo['NumTempSenChan']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Number of Flow Control Channels:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['NumFlowCntlChan']; ?></p>
+                    <p class="span3"><?=$systemInfo['NumFlowCntlChan']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Number of Analog Channels:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['NumAnlgChan']; ?></p>
+                    <p class="span3"><?=$systemInfo['NumAnlgChan']?></p>
                 </div>
 
                 <hr>
 
                 <div class="row">
                     <p class="span3"><strong>Number of RSM's:</strong></p>
-                    <p class="span3"><?php echo $systemInfo['NumofRSM']; ?></p>
+                    <p class="span3"><?=$systemInfo['NumofRSM']?></p>
                 </div>
 
                 <hr>
@@ -166,7 +178,6 @@ require_once('../includes/header.php');
                     <p class=\"span3\"><strong>Location of RSM " . ($i + 1) . ":</strong></p>
                     <p class=\"span3\">" . $systemInfo[$rsmNum] . "</p>
                 </div>";
-                        echo  $systemInfo['LocationRSM" . ($i + 1) . "'];
                     }
                 ?>
 
