5d8ea3ea7709c44781b6b069ec2bc83a51f2a4a0
Geoff Young <geoffreysyoung@gmail.com>
2012-10-24 22:52:16 -0400

    Fixed links and SysID

--- setup/index.php
+++ setup/index.php
@@ -11,28 +11,28 @@ checkSystemSet($config);
 
 require_once('../includes/header.php');
 
- $SysID=$_Session["SysID"];     
-        
+ $SysID=$_SESSION["SysID"];
+
  ?>
         <div class="row">
             <h1 class="span8 offset2">System Setup</h1>
         </div>
         <div class="row">
-            <h2 class="span8 offset3"><a href="Information/">Building Information</a></h2>
+            <h2 class="span8 offset3"><a href="information/">Building Information</a></h2>
         </div>
         <div class="row">
-        <h2 class="span8 offset3"><a href="<?php echo("../setup/Sensor_Mapping/index.php?Sysid=".$SysID);?>">Sensor Mapping</a></h2>
+        <h2 class="span8 offset3"><a href="sensor_mapping?id=<?php echo $SysID; ?>">Sensor Mapping</a></h2>
         </div>
         <div class="row">
             <h2 class="span8 offset3">Web Chart Mapping</h2>
         </div>
         <div class="row">
-            <h2 class="span8 offset3"><a href="Alarm_Limits/">Alarm Limits</a></h2>
+            <h2 class="span8 offset3"><a href="alarm_aimits/">Alarm Limits</a></h2>
         </div>
         <div class="row">
             <h2 class="span8 offset3">Maintenance</h2>
         </div>
-        
+
 <?php
 require_once('../includes/footer.php');
 ?>
