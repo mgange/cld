669c10d919963a038458e0feb9808e79a29f8927
Geoff Young <geoff.young@hvtdc.org>
2012-11-19 17:10:21 -0500

    Added wuery and export to excel file

--- datadownload/index.php
+++ datadownload/index.php
@@ -27,6 +27,132 @@ require_once('../includes/pageStart.php');
 
 checkSystemSet($config);
 
+$db = new db($config);
+
+if(isset($_POST['from']) && isset($_POST['until'])) {
+
+    require_once('class.excelXML.php');
+
+    $bind[':SysID'] = $_SESSION['SysID'];
+    $bind[':from'] = date('Y-m-d', strtotime($_POST['from']));
+    $bind[':until'] = date('Y-m-d', strtotime($_POST['until']));
+
+    $query = "SELECT
+    SourceHeader.Recnum AS ID,
+    SourceHeader.DateStamp AS Date,
+    SourceHeader.TimeStamp AS Time,
+    SourceData0.*
+    FROM SourceHeader, SourceData0
+    WHERE SysID = :SysID
+    AND SourceHeader.Recnum = SourceData0.HeadID
+    AND SourceHeader.DateStamp >= :from
+    AND SourceHeader.DateStamp < :until
+    ORDER BY SourceHeader.DateStamp DESC, SourceHeader.TimeStamp DESC
+    LIMIT 0,10
+    ";
+    $main = $db->fetchAll($query, $bind);
+
+    $query = "SELECT
+    SourceHeader.Recnum AS ID,
+    SourceHeader.DateStamp AS Date,
+    SourceHeader.TimeStamp AS Time,
+    SourceData1.*
+    FROM SourceHeader, SourceData1
+    WHERE SysID = :SysID
+    AND SourceHeader.Recnum = SourceData1.HeadID
+    AND SourceHeader.DateStamp >= :from
+    AND SourceHeader.DateStamp < :until
+    ORDER BY SourceHeader.DateStamp DESC, SourceHeader.TimeStamp DESC
+    LIMIT 0,10
+    ";
+    $rsm = $db->fetchAll($query, $bind);
+
+    $query = "SELECT
+    SourceHeader.Recnum AS ID,
+    SourceHeader.DateStamp AS Date,
+    SourceHeader.TimeStamp AS Time,
+    SourceData4.*
+    FROM SourceHeader, SourceData4
+    WHERE SysID = :SysID
+    AND SourceHeader.Recnum = SourceData4.HeadID
+    AND SourceHeader.DateStamp >= :from
+    AND SourceHeader.DateStamp < :until
+    ORDER BY SourceHeader.DateStamp DESC, SourceHeader.TimeStamp DESC
+    LIMIT 0,10
+    ";
+    $modbus = $db->fetchAll($query, $bind);
+
+    // Create new object
+    $excel = new excel_xml();
+
+    //Define styles for header rows
+    $header_style = array(
+        'size'       => '12',
+        'color'      => '#ffffff',
+        'bgcolor'    => '#aaaaff'
+    );
+    $excel->add_style('header', $header_style);
+
+    // MAIN VALUES
+
+    // Create an empty array for header row values
+    $headers = array();
+
+    // Added the key names to the header row
+    foreach($main[0] as $key => $val) {
+        array_push($headers, $key);
+    }
+    $excel->add_row($headers, 'header');
+
+    // Write each DB record as a spreadsheet row
+    foreach($main as $row) {
+        $excel->add_row($row);
+    }
+
+    // Put all those rows in a worksheet
+    $excel->create_worksheet('Main');
+
+    // RSM VALUES
+
+    // Create an empty array for header row values
+    unset($headers);
+    $headers = array();
+
+    // Added the key names to the header row
+    foreach($rsm[0] as $key => $val) {
+        array_push($headers, $key);
+    }
+    $excel->add_row($headers, 'header');
+
+    // Write each DB record as a spreadsheet row
+    foreach($rsm as $row) {
+        $excel->add_row($row);
+    }
+    $excel->create_worksheet('RSM');
+
+    // MODBUS GATEWAY VALUES
+
+    // Create an empty array for header row values
+    unset($headers);
+    $headers = array();
+
+    // Added the key names to the header row
+    foreach($rsm[0] as $key => $val) {
+        array_push($headers, $key);
+    }
+    $excel->add_row($headers, 'header');
+
+    // Write each DB record as a spreadsheet row
+    foreach($modbus as $row) {
+        $excel->add_row($row);
+    }
+    $excel->create_worksheet('Modbus Gateway');
+
+
+    $xml = $excel->generate();
+    $excel->download('Download.xls');
+}
+
 require_once('../includes/header.php');
 
 $from = date("Y-m-d", time() - 60 * 60 * 24);
