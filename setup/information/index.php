44d81b2e89d9d90216154797b3450b49a764e92f
Bob Incerto <bob.incerto@gmail.com>
2012-11-19 15:35:28 -0500

    Lots of changes
    who knows what, it's just committed in chunks now

--- setup/information/index.php
+++ setup/information/index.php
@@ -1,120 +1,218 @@
+
+
 <?php
 /**
  *------------------------------------------------------------------------------
- * Information Index Page
+ *  Building Information Setup Index Page
  *------------------------------------------------------------------------------
  *
- */
-require_once('../../includes/pageStart.php');
+ *
+**/
 
-checkSystemSet($config);
+function SelectPD($DBValue,$SelValue)
+{
+    if ($DBValue==$SelValue) { return "selected";} else {return "";}
+}
+function MySQL_Pull_Down($config,$query,$InputName,$DisplayField,$SelField,$SelValue,$DefMess,$Class,$Submit)
+{
+    // first get data
+  $dbpd = new db($config);
+  $pdrows= $dbpd -> numRows($query);
+  $PDList = $dbpd -> fetchAll($query);
+  $dropdown = "<select name='".$InputName."' class='".$Class."'>";
+   
+foreach ($PDList as $row) {
+    echo($row[$SelField]."||".$SelValue);
+    $Sel= SelectPD($row[$SelField],$SelValue);
+  echo($Sel);
+   $dropdown .= "\r\n<option value='".$row[$SelField]."' ".$Sel.">".$row[$DisplayField]."</option>";
+    }
 
-if($_SESSION['authLevel'] < 3) {
+   $dropdown .= "\r\n</select>";
+
+   echo $dropdown;
+
+}
+
+// check to see if this is a new system setup
+ if (isset($_SESSION['SetUpNew'])) { $NewSystem=$_SESSION['SetUpNew'];} else {$NewSystem=false;}
+ // if not a new system set up check to see if a system has been selected if not refers to choose system option
+if  (NewSystem==false) {checkSystemSet($config);}
+// goes to home page if trying to set up new system without proper authorization
+if($_SESSION['authLevel'] < 3 && $NewSystem==false) {
     gtfo($config);
 }
 
-$db = new db($config);
-$SysId = $_SESSION['SysID'];
-$BuildingID = $_SESSION['buildingID'];
-$query = "SELECT * FROM SystemConfig WHERE SysId = $SysId AND BuildingID = $BuildingID";
-$systemInfo = $db -> fetchRow($query);
-$query = "SELECT * FROM buildings WHERE buildingID = $BuildingID";
-$buildingInfo = $db -> fetchRow($query);
-$query = "SELECT * FROM customers WHERE customerID = $buildingInfo[CustomerID]";
-$customerInfo = $db -> fetchRow($query);
+ //$_SESSION['BuildStart']=true;
 
-if(count($_POST) > 0) {
-    $query = "UPDATE customers,SystemConfig SET customers.email1 = :email1, customers.email2 = :email2, SystemConfig.Maintainer = :maintainer WHERE customers.customerID = $customerInfo[customerID] AND SystemConfig.SysId = $SysId AND SystemConfig.BuildingID = $BuildingID";
-    $bind[':email1'] = $_POST['email1'];
-    $bind[':email2'] = $_POST['email2'];
-    $bind[':maintainer'] = $_POST['maintainer'];
+// existing system modifications
+if ($NewSystem==false)
+{
 
-    if($db -> execute($query, $bind)) {
-        header('Location: ../?a=ss'); //a = Alert  ss = Secondary Success(generic)
-    }else{
-        header('Location: ./?a=e'); //a = Alert  e = error(generic)
+
+    $SysId = $_SESSION['SysID'];
+    $BuildingID = $_SESSION['buildingID'];
+    $query = "SELECT * FROM SystemConfig WHERE SysId = $SysId AND BuildingID = $BuildingID";
+    $systemInfo = $db -> fetchRow($query);
+    $query = "SELECT * FROM buildings WHERE buildingID = $BuildingID";
+    $buildingInfo = $db -> fetchRow($query);
+    $query = "SELECT * FROM customers WHERE customerID = $buildingInfo[CustomerID]";
+    $customerInfo = $db -> fetchRow($query);
+    $SysName=" for ".$systemInfo['SysName'];
+// posts updates for existing systems
+    if(count($_POST) > 0) {
+     //   $_SESSION['SystemStart']=false;
+   //     $query = "UPDATE customers,SystemConfig SET customers.email1 = :email1, customers.email2 = :email2, SystemConfig.Maintainer = :maintainer WHERE customers.customerID = $customerInfo[customerID] AND SystemConfig.SysId = $SysId AND SystemConfig.BuildingID = $BuildingID";
+   //     $bind[':email1'] = $_POST['email1'];
+    //    $bind[':email2'] = $_POST['email2'];
+    //    $bind[':maintainer'] = $_POST['maintainer'];
+
+    //    if($db -> execute($query, $bind)) {
+     //       header('Location: ../?a=ss'); //a = Alert  ss = Secondary Success(generic)
+     //    }else{
+     //       header('Location: ./?a=e'); //a = Alert  e = error(generic)
+    //    }
+
+ //   die(require_once('../../includes/footer.php'));
     }
 
-    die(require_once('../../includes/footer.php'));
+//require_once('../../includes/header.php');
+}
+else  // new system set up
+{
+    $Readonly=false;
+    // first determine next available SYSID
+    $SysIDQuery ="Select SysID from SystemConfig order by SysID desc limit 1";
+    $NextID = $db -> fetchRow($SysIDQuery);
+    $NewID=$NextID['SysID']+1;
+    $SysName="New SysID= ".$NewID;
 }
 
-require_once('../../includes/header.php');
-
+//parameter feedback
+ $BldSel=$_POST['Building'];
+ $ConfigSel=$_POST['Configuration'];
+ $SysDescrpSel=$_POST['SysDescription'];
+ $SysTypeSel=$_POST['SysType'];
+ $PlatformIDSel=$_POST['PlatformID'];
+ $HeatExhangerSel=$_POST['HeatExchanger'];
+ $InstallDateSel=$_POST['InstallDate'];
+ $InstallerSel=$_POST['Installer'];
+ $MaintainerSel=$_POST['Maintainer'];
+ $LocofMainSel=$_POST['LocofMain'];
+ $DAMIDSel=$_POST['DAMID'];
+ $NoofRSMSSel=$_POST['NoofRSMS'];
+ $NoofThermsSel=$_POST['NoofTherms'];
+ $NoofPowerSel=$_POST['NoofPower'];
+ 
+ echo("!!=".$ConfigSel);
 ?>
 
 <div class="row">
-    <h1 class="span8 offset2">'<?php echo $systemInfo['SysName']; ?>' Information</h1>
+    <h2 class="span8 offset2"> System Information for <?php echo $SysName; ?></h2>
 </div>
 
+
 <form action="./" method="POST">
-    <div class="row">
-        <div class="span6">
-            <label for="name">Customer Name
-                <input readonly="true" type="text" class="span6" value="<?=$customerInfo['customerName']?>">
+
+    <div class="row" >
+        <div class="span5">
+            <label for="name" alt="test">Building ID and Location of System
+                <?php
+ 
+                    $query="Select buildingID,buildingName from buildings order by NumSysConfig,buildingID";
+                    MySQL_Pull_Down($config,$query,"Building","buildingName","buildingID",$BldSel,"","span5","");
+                ?>
+
             </label>
-            <label for="cLocation">Customer Location
-                <textarea style="resize:none" readonly="true" class="span6" rows="<?=isset($customerInfo['addr2']) ? 3 : 2?>"><?=$customerInfo['addr1'] . "\n" . (!empty($customerInfo['addr2']) ? $customerInfo['addr2'] . "\n" : "") . $customerInfo['city'] . ", " . $customerInfo['state'] . " " . $customerInfo['zip']?></textarea>
+              <label for="Configuration">Configuration
+                  <?php
+echo($ConfigSel);
+                    $query="Select DefaultValue,ItemName from SysConfigDefaults where ConfigGroup='Configuration' order by DefaultValue";
+                    MySQL_Pull_Down($config,$query,"Configuration","ItemName","DefaultValue",$ConfigSel,"","span5","");
+                  ?>
             </label>
-            <label for="email1">Customer Email
-                <input name="email1" type="text" class="span6" value="<?=$customerInfo['email1']?>">
+            <label for="SysDescription">System Description
+                <input name="SysDescription" type="text" class="span5" value="<?php echo $SysDescrpSel; ?>">
             </label>
-            <label for="email2">Alternate Email
-                <input name="email2" type="text" class="span6" value="<?=$customerInfo['email2']?>">
+
+
+
+            <label for="InstallDate">Install Date
+                <input  name="InstallDate" type="text" class="span5" value="<?php echo $InstallDateSel; ?>">
             </label>
-            <label for="bLocation">Building Location
-                <textarea readonly="true" style="resize:none" class="span6" rows="<?=isset($buildingInfo['address2']) ? 3 : 2?>"><?=$buildingInfo['address1'] . "\n" . (!empty($buildingInfo['address2']) ? $buildingInfo['address2'] . "\n" : "") . $buildingInfo['city'] . ", " . $buildingInfo['state'] . " " . $buildingInfo['zip']?></textarea>
+
+            <label for="Maintainer">Maintainer
+               <?php
+                    $query="Select Company from MaintainResource where Category='Maintainer'";
+                    MySQL_Pull_Down($config,$query,"Maintainer","Company","Company",$MaintainerSel,"","span5","");
+                 ?>
             </label>
-            <label for="installDate">Install Date
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['InstallDate']?>">
+             <HR>
+            <label for="DAMID">DAMID
+                <input name="DAMID" type="text" class="span5" value="<?php echo $DAMIDSel; ?>">
             </label>
-            <label for="installer">Installer
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['Installer']?>">
-            </label>
-            <label for="maintainer">Maintainer
-                <input name="maintainer" type="text" class="span6" value="<?=$systemInfo['Maintainer']?>">
+            <label for="NoofTherms">Number of Thermostats
+                <input name="NoofTherms" type="text" class="span5" value="<?php echo $NoofThermsSel; ?>">
             </label>
         </div>
-        <div class="span6">
-            <label for="maintainer">DAMID
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['DAMID']?>">
+
+
+
+        <div class="row" >
+           <div class="span5">
+            <label for="SystemType">System Type
+                <?php
+                  $query="Select ItemName,DefaultValue from SysConfigDefaults where ConfigGroup='Systemtype' order by DefaultValue";
+                    MySQL_Pull_Down($config,$query,"Configuration","ItemName","DefaultValue",$ConfigSel,"","span5","");
+                 ?>
             </label>
-            <label for="sysType">System Type
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['Systype']?>">
+            <label for="PlatformID">Platform ID
+               <?php
+                  $query="Select ItemName,DefaultValue from SysConfigDefaults where ConfigGroup='PlatformID' order by DefaultValue";
+                    MySQL_Pull_Down($config,$query,"PlatformID","ItemName","DefaultValue",$PlatformIDSel,"","span5","");
+                 ?>
             </label>
-            <label for="config">Configuration
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['Configuration']?>">
+             <label for="HeatExchange">Heat Exchange Unit
+                 <?php
+                  $query="Select ItemName,DefaultValue from SysConfigDefaults where ConfigGroup='HeatExchanger' order by DefaultValue";
+                   MySQL_Pull_Down($config,$query,"Configuration","ItemName","DefaultValue",$HeatExchangeSel,"","span5","");
+                 ?>
             </label>
-            <label for="heatExchange">Heat Exchange Unit
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['HeatExchangeUnit']?>">
+             <label for="Installer">Installer
+                <?php
+                  $query="Select Company from MaintainResource where Category='Installer' ";
+                  MySQL_Pull_Down($config,$query,"Company","Company","Company",$InstallerSel,"","span5","");
+                 ?>
             </label>
-            <label for="NumofSensGrp">Number of Sensor Group
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['NumofSensGrp']?>">
+
+            <label for="LocofMain">Location of Main System
+                <input name="LocofMain" type="text" class="span5" value="<?php echo $LocofMainSel; ?>">
             </label>
-            <label for="NumDigSenChan">Number of Digital Sensor Channels
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['NumDigSenChan']?>">
+
+
+             <hr>
+
+            <label for="NoofRSMS">Number of RSMs
+                <input name="NoofRSMS" type="text" class="span5" value="<?php echo $NoofRSMSSel; ?>">
             </label>
-            <label for="NumTempSenChan">Number of Temperature Sensor Channels
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['NumTempSenChan']?>">
-            </label>
-            <label for="NumFlowCntlChan">Number of Flow Control Channels
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['NumFlowCntlChan']?>">
-            </label>
-            <label for="NumAnlgChan">Number of Analog Channels
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['NumAnlgChan']?>">
-            </label>
-            <label for="NumTempSenChan">Number of RSM's
-                <input readonly="true" type="text" class="span6" value="<?=$systemInfo['NumofRSM']?>">
-            </label>
+
             <?php
-                for($i=0;$i<$systemInfo['NumofRSM'];$i++){
-                    $rsmNum = "LocationRSM" . ($i + 1);
-                    echo "<label for=\"locationRSM . ($i+1) . \">Location of RSM " . ($i + 1) . "
-                        <input readonly=\"true\" type=\"text\" class=\"span6\" value=\"" . $systemInfo[$rsmNum] . "\">";
+             //   for($i=0;$i<$systemInfo['NumofRSM'];$i++){
+            //        $rsmNum = "LocationRSM" . ($i + 1);
+             //       echo "<label for=\"locationRSM . ($i+1) . \">Location of RSM " . ($i + 1) . "
+             //           <input readonly=\"true\" type=\"text\" class=\"span6\" value=\"" . $systemInfo[$rsmNum] . "\">";
                     //echo $systemInfo['$rsmNum'];
-                }
+         //       }
             ?>
+
+
+             <label for="NoofPower">Number of Power Meters
+                <input  name="NoofPower" type="text" class="span5" value="<?php echo $NoofPowerSel; ?>">
+            </label>
         </div>
-    </div>
+       </div>
+
+
     <div class="row">
         <div class="span10 offset1">
             <button type="submit" class="btn btn-success">
@@ -127,9 +225,10 @@ require_once('../../includes/header.php');
             </a>
         </div>
     </div>
+ </div>
     <input type="hidden" name="customerID" value="<?=$customerInfo['customerID']?>">
 </form>
 
 <?php
-require_once('../../includes/footer.php');
+//require_once('../../includes/footer.php');
 ?>
