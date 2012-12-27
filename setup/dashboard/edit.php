ed2193d7ef773712b1a28fe28529b17beb0fe0ca
James Lynch <james.lynch@hvtdc.org>
2012-12-27 16:56:29 -0500

    Dashboard functionable

--- setup/dashboard/edit.php
+++ setup/dashboard/edit.php
@@ -9,20 +9,94 @@ require_once('../../includes/pageStart.php');
 
 checkSystemSet($config);
 
-$systemID = $_SESSION['SysID'];
-$buildingID = $_SESSION['buildingID'];
+$systemID = $_GET['sys'];
+//$buildingID = $_SESSION['buildingID'];
 
 $db = new db($config);
+
+if(count($_POST)){
+    //query to check if x y was changed
+    $query = "SELECT * FROM WebRefTable WHERE PageLocX = " . $_POST['xpos'] . " AND PageLocY = " . $_POST['ypos'] . " AND WebSensRefNum = " . $_POST['WebSensRefNum'] . " AND WebSubPageName = '" . $_POST['WebPage'] . "'";
+    //if changed add an unique to the webreftable
+    if($db -> numRows($query) == 0){
+        //duplicate row first then update
+        $query = "SELECT * FROM WebRefTable WHERE WebSensRefNum = " . $_POST['WebSensRefNum'] . " AND WebSubPageName = '" . $_POST['WebPage'] . "'";
+        $sth = $db -> prepare($query);
+        $sth -> execute();
+        $result = $sth -> fetch(PDO::FETCH_NUM);
+        $query = "INSERT INTO WebRefTable VALUES(NULL, ";
+        for($i=1;$i<count($result);$i++) $query .= (isset($result[$i]) ? "'" . $result[$i] . "', " : "NULL, ");
+        //remove last , with )
+        $query = substr_replace($query,")",strlen($query) - 2);
+        $db -> execute($query);
+        $lastinsert = $db -> lastInsertId();
+        $query = "UPDATE WebRefTable SET SysID = " . $systemID . ", PageLocX = " . $_POST['xpos'] . ", PageLocY = " . $_POST['ypos'] . ", WebSubPageName = '" . $_POST['WebPage'] . "' WHERE Recnum = " . $lastinsert;
+        $db -> execute($query);
+    }
+
+    //grab the websensrefnum either just inserted or one there
+    //check if sysmap row was changed
+    $query = "SELECT * FROM SysMap WHERE SourceID = " . (isset($_POST['sourceID']) ? $_POST['sourceID'] : "4")
+            . " AND SensorStatus = " . (isset($_POST['sensorStatus']) ? $_POST['sensorStatus'] : "2")
+            . " AND AlarmUpLimit " . (($_POST['highLimit'] != "") ? "= " . $_POST['highLimit'] : "IS NULL")
+            . " AND AlarmLoLimit " . (($_POST['lowLimit'] != "") ? "= " . $_POST['lowLimit'] : "IS NULL")
+            . " AND AlertPercent = " . $_POST['percentWarn'] . " AND AlarmTrigger = " . $_POST['alarmTrigger']
+            . " AND WebSensRefNum = " . $_POST['WebSensRefNum'];
+            echo $query . "<br>";
+    if($db -> numRows($query) == 0){
+        //check if already an unique
+        $query = "SELECT * FROM SysMap WHERE WebSensRefNum = " . $_POST['WebSensRefNum'] . " AND SysID = " . $systemID;
+        //die($query);
+        if($db -> numRows($query) == 0){
+            //add
+            //duplicate row first then update
+            $query = "SELECT * FROM SysMap WHERE WebSensRefNum = " . $_POST['WebSensRefNum'];
+            $sth = $db -> prepare($query);
+            $sth -> execute();
+            $result = $sth -> fetch(PDO::FETCH_NUM);
+            $query = "INSERT INTO SysMap VALUES(NULL, ";
+            for($i=1;$i<count($result);$i++) $query .= (isset($result[$i]) ? "'" . $result[$i] . "', " : "NULL, ");
+            //remove last , with )
+            $query = substr_replace($query,")",strlen($query) - 2);
+            $db -> execute($query);
+            $lastinsert = $db -> lastInsertId();
+            $query = "UPDATE SysMap SET DAMID = '" . $_POST['DAMID'] ."', SysID = " . $systemID . ", SourceID = " . (isset($_POST['sourceID']) ? $_POST['sourceID'] : "4")
+                     . ", AlarmUpLimit = " . (($_POST['highLimit'] == "") ? "NULL" : $_POST['highLimit'])
+                     . ", AlarmLoLimit = " . (($_POST['lowLimit'] == "") ? "NULL" : $_POST['lowLimit'])
+                     . ", AlertPercent = " . $_POST['percentWarn'] . ", AlarmTrigger = " . $_POST['alarmTrigger']
+                     . " WHERE Recnum = " . $lastinsert;
+            $db -> execute($query);
+        }else{
+            //update
+            $query = "UPDATE SysMap SET SourceID = " . (isset($_POST['sourceID']) ? $_POST['sourceID'] : "4")
+                     . ", AlarmUpLimit = " . (($_POST['highLimit'] == "") ? "NULL" : $_POST['highLimit'])
+                     . ", AlarmLoLimit = " . (($_POST['lowLimit'] == "") ? "NULL" : $_POST['lowLimit'])
+                     . ", AlertPercent = " . $_POST['percentWarn'] . ", AlarmTrigger = " . $_POST['alarmTrigger']
+                     . " WHERE SysID = " . $systemID . " AND WebSensRefNum = " . $_POST['WebSensRefNum'];
+                     echo $query;
+            $db -> execute($query);
+        }
+    }
+    //if changed check if uniqe value is there and update or insert
+
+    echo "<script type=\"text/javascript\">window.close()</script>";
+}
 
 if((!isset($_GET['id'])) || (!isset($_GET['sys']))) header("Location: ../../");
 $query = "SELECT * FROM SystemConfig WHERE SysID = " . $_GET['sys'];
 $systemInfo = $db -> fetchRow($query);
 
-$query = "SELECT A.SensorLabel,A.PageLocX,A.PageLocY,
-            B.Recnum,B.SysGroup,B.SourceID,B.SensorType,B.SensorStatus,
+$queryTemp = "SELECT A.WebSensRefNum,A.SensorLabel,A.PageLocX,A.PageLocY,
+            B.SysGroup,B.SourceID,B.SensorType,B.SensorStatus,
             B.AlarmUpLimit,B.AlarmLoLimit,B.AlertPercent,B.AlarmTrigger
             FROM WebRefTable AS A, SysMap AS B
-            WHERE B.Recnum = " . $_GET['id'];
+            WHERE A.WebSensRefNum = " . $_GET['id'] . " AND B.WebSensRefNum = " . $_GET['id'];
+if(!isset($_GET['rsm'])) $queryTemp .= " AND A.WebSubPageName = 'Main'";
+else $queryTemp .= " AND A.WebSubPageName = 'RSM'";
+$query = $queryTemp . " AND A.SysID = " . $systemID . " AND B.SysID = " . $systemID;
+
+//if no uniques, use default
+if($db -> numRows($query) == 0) $query = $queryTemp . " AND A.SysID = 0 AND B.SysID = 0";
 $result = $db -> fetchRow($query);
 
 $sensorStatus = $result['SensorStatus'];
@@ -31,37 +105,58 @@ $sensorStatus = $result['SensorStatus'];
 ?>
 <link rel="stylesheet" href="<?php echo $_SESSION['base_domain'] . $_SESSION['base_dir']; ?>css/main.css">
 
-<form>
+<script type="text/javascript">
+    function SensorStatus(value){
+        if(value != ""){
+            document.getElementsByName("sensorStatus")[0].disabled = "";
+            document.getElementsByName("sensorStatus")[0].options["hide"].style.display = "none";
+            document.getElementsByName("sensorStatus")[0].value = 1;
+        }else{
+            highVal = document.getElementsByName("highLimit")[0].value;
+            lowVal = document.getElementsByName("highLimit")[0].value;
+            if((highVal == "") & (lowVal == "")){
+                document.getElementsByName("sensorStatus")[0].disabled = "disabled";
+                document.getElementsByName("sensorStatus")[0].options["hide"].style.display = "";
+                document.getElementsByName("sensorStatus")[0].value = 2;
+            }
+        }
+    }
+</script>
+
+<form action="" method="post">
     <h2 class="offset1"><?=$result['SensorLabel']?></h2>
     <div class="row">
         <div class="span5 offset1">
             <label for="sourceID">Source ID<br>
-                <select name="sourceID"<?=($result['SourceID'] == 4) ? " disabled=\"disabled\"" : ""?>>
-                    <?php
-                        for($i=0;$i<$systemInfo['NumofRSM'];$i++){
+                <?php
+                    if(($result['SensorType'] == 6) || ($result['SensorType'] == 7)){
+                        echo "<select name=\"sourceID\" disabled=\"disabled\">";
+                        echo "<option value=\"4\">4</option>";
+                    }else{
+                        echo "<select name=\"sourceID\">";
+                        for($i=0;$i<=$systemInfo['NumofRSM'];$i++){
                             $select = "";
                             if($i == $result['SourceID']) $select = " selected=\"selected\"";
-                            if($i == 4){
-                                echo "<option value=\"" . $i . "\">" . $i . "</option>";
-                                echo "<option value=\"" . ($i + 1) . "\">" . ($i + 1) . "</option>";
-                            }else{
-                                if($i > 4) echo "<option value=\"" . ($i + 1) . "\">" . ($i + 1) . "</option>";
-                                else echo "<option value=\"" . $i . "\"" . $select . ">" . $i . "</option>";
-                            }
+                            if($i >= 4){
+                                echo "<option value=\"" . ($i + 1) . "\"" . $select . ">" . ($i + 1) . "</option>";
+                            }else echo "<option value=\"" . $i . "\"" . $select . ">" . $i . "</option>";
                         }
-                        if($i <= 4){
-                            if($result['SourceID'] == 4) echo "<option value=\"4\" selected=\"selected\">4</option>";
-                            else echo "<option value=\"4\">4</option>";
-                        }
-                    ?>
-                </select>
+                    }
+                    echo "</select>";
+                ?>
             </label>
             <?php if($result['SourceID'] == 4){ ?>
-                <label for="sysGroup">Meter<br>
+                <label for="sysGroup"><?php
+                        if($result['SensorType'] == 6){
+                            $max = $systemInfo['NumofPowers'];   //power meter
+                            echo "Power Meter<br>";
+                        }elseif($result['SensorType'] == 7){
+                            $max = $systemInfo['NumofTherms'];   //therm meter
+                            echo "Thermostat<br>";
+                        }
+                    ?>
                     <select name="sysGroup">
                         <?php
-                            if($result['SensorType'] == 6) $max = $systemInfo['NumofPowers'];   //power meter
-                            elseif($result['SensorType'] == 7) $max = $systemInfo['NumofTherms'];   //therm meter
                             for($i=1;$i<=$max;$i++){
                                 $select = "";
                                 if($i == $result['SysGroup']) $select = " selected=\"selected\"";
@@ -72,26 +167,33 @@ $sensorStatus = $result['SensorStatus'];
                 </label>
             <?php } ?>
             <label for="sensorStatus">Sensor Alarm<br>
-                <select name="sensorStatus">
+                <select name="sensorStatus"<?=($sensorStatus == 2) ? " disabled=\"disabled\"" : ""?>>
                     <?php $select = "selected=\"selected\""; ?>
-                    <option value="0"<?=($sensorStatus == 0) ? $select : ""?>>Inhibited</option>
+                    <option value="0"<?=($sensorStatus == 0) ? $select : ""?>>Alarm Off & Hidden</option>
                     <option value="1"<?=($sensorStatus == 1) ? $select : ""?>>Alarm On</option>
+                    <option name="hide" value="2"<?=($sensorStatus == 2) ? $select : ""?>>Never Alarmed</option>
                     <option value="3"<?=($sensorStatus == 3) ? $select : ""?>>Alarm Off</option>
                 </select>
+            </label>
+            <label for="xpos">X-Position<br>
+                <input type="text" class="span2" style="height:30px" name="xpos" value="<?=$result['PageLocX']?>">
+            </label>
+            <label for="ypos">Y-Position<br>
+                <input type="text" class="span2" style="height:30px" name="ypos" value="<?=$result['PageLocY']?>">
             </label>
         </div>
         <div class="span5">
             <label for="highLimit">Alarm High Limit<br>
-                <input type="text" class="span2" name="highLimit" value="<?=$result['AlarmUpLimit']?>">
+                <input type="text" class="span2" style="height:30px" name="highLimit" value="<?=$result['AlarmUpLimit']?>" onkeyup="SensorStatus(this.value)">
             </label>
             <label for="lowLimit">Alarm Low Limit<br>
-                <input type="text" class="span2" name="lowLimit" value="<?=$result['AlarmLoLimit']?>">
+                <input type="text" class="span2" style="height:30px" name="lowLimit" value="<?=$result['AlarmLoLimit']?>" onkeyup="SensorStatus(this.value)">
             </label>
             <label for="percentWarn">Alarm Warning Percentage<br>
-                <input type="text" class="span2" name="percentWarn" value="<?=$result['AlertPercent']?>">
+                <input type="text" class="span2" style="height:30px" name="percentWarn" value="<?=$result['AlertPercent']?>">
             </label>
             <label for="alarmTrigger">Alarm After<br>
-                <input type="text" class="span2" name="alarmTrigger" value="<?=$result['AlarmTrigger']?>"> minutes
+                <input type="text" class="span2" style="height:30px" name="alarmTrigger" value="<?=$result['AlarmTrigger']?>"> minutes
             </label>
         </div>
         <div class="row">
@@ -107,5 +209,8 @@ $sensorStatus = $result['SensorStatus'];
                 </a>
             </div>
         </div>
+        <input type="hidden" name="DAMID" value="<?=$systemInfo['DAMID']?>">
+        <input type="hidden" name="WebSensRefNum" value="<?=$result['WebSensRefNum']?>">
+        <input type="hidden" name="WebPage" value="<?=isset($_GET['rsm']) ? 'RSM' : 'Main'?>">
     </div>
 </form>
--- setup/dashboard/index.php
+++ setup/dashboard/index.php
@@ -22,9 +22,12 @@ $numOfRSM = $result['NumofRSM'];
 $numOfPowers = $result['NumofPowers'];
 $numOfTherms = $result['NumofTherms'];
 
-
-if(isset($_GET['z'])) $zone = "RSM" . $_GET['z'];
-else $zone = "Main";
+$zone = "Main";
+$rsm = 0;
+if(isset($_GET['z'])){
+    $zone = "RSM" . $_GET['z'];
+    $rsm = $_GET['z'];
+}
 
 require_once('../../includes/header.php');
 ?>
@@ -41,79 +44,69 @@ require_once('../../includes/header.php');
 </div>
 
 <div class="row">
-    <form action="./" method="post">
-        <div class="status-container span10 offset1">
-            <div class="status-Back map">
-                <img src="../../status/image/WebBackGroundHeatingMode.png" alt="Heat Exchanger">
-            </div>
+    <div class="status-container span10 offset1">
+        <div class="status-Back map">
+            <img src="../../status/image/WebBackGroundHeatingMode.png" alt="Heat Exchanger">
+        </div>
+        <?php
+            echo "<div class=";
+            if($configuration == 1){
+                echo "\"status-OpenLoopDryWell\">";
+                echo "<img src=\"../../status/image/WebOpenLoopDryWell.png\" alt=\"Open Loop Dry Well\">";
+            }elseif($configuration == 2){
+                echo "\"status-OpenLoop\">";
+                echo "<img src=\"../../status/image/WebOpenLoop.png\" alt=\"Open Loop\">";
+            }elseif($configuration == 3){
+                echo "\"status-ClosedLoop\">";
+                echo "<img src=\"../../status/image/WebClosedLoop.png\" alt=\"Closed Loop\">";
+            }else echo "\"\">";
+            echo "</div>";
+        ?>
+        <?php
+            $query = "SELECT A.SensorLabel,A.PageLocX,A.PageLocY,B.SensorColName,B.WebSensRefNum
+                    FROM WebRefTable AS A, SysMap AS B
+                    WHERE A.WebSensRefNum = B.WebSensRefNum AND A.Inhibit = 0 AND B.SysID = 0";
+            if(!strcasecmp($zone,"Main")) $query .= " AND A.WebSubPageName = 'Main'";
+            else $query .= " AND A.WebSubPageName = 'RSM'";
+
+            $ref = $db -> fetchAll($query);
+            foreach($ref as $result){
+                //check uniques and use if possible
+                $query = "SELECT A.SensorLabel,A.PageLocX,A.PageLocY,B.SensorColName,B.WebSensRefNum
+                        FROM WebRefTable AS A, SysMap AS B
+                        WHERE A.SensorLabel = '" . $result['SensorLabel'] . "' AND A.WebSensRefNum = B.WebSensRefNum AND A.Inhibit = 0 AND B.SysID = " . $systemID;
+                if(!strcasecmp($zone,"Main")) $query .= " AND A.WebSubPageName = 'Main'";
+                else $query .= " AND A.WebSubPageName = 'RSM'";
+                $unique = $db -> fetchRow($query);
+                if($db -> numRows($query) != 0) $result = $unique;
+                $xpos = $result['PageLocX'];
+                $ypos = $result['PageLocY'];
+                $label = $result['SensorLabel'];
+                $id = $result['WebSensRefNum'];
+        ?>
+                <p class="label-status" style="top:<?=$ypos-20?>px;left:<?=$xpos+5?>px;"><?=$label?></p>
+                <p class="value-status" style="top:<?=$ypos?>px;left:<?=$xpos?>px;line-height:1.2em">
+                    <a href="javascript:void(0);" class="icon-pencil" title="Edit"
+                        onclick="window.open(
+                            'edit.php?sys=<?=$systemID?><?=($rsm != 0) ? "&rsm=" . $rsm : ""?>&id=<?=$id?>',
+                            'Popup','width=800,height=450,dependent=yes,0,status=0,resizable=1');">
+                    </a>
+                </p>
+        <?php
+            }
+        ?>
+        <div class="span10" style="margin-left:50px">
+            <p style="float:right">
             <?php
-                echo "<div class=";
-                if($configuration == 1){
-                    echo "\"status-OpenLoopDryWell\">";
-                    echo "<img src=\"../../status/image/WebOpenLoopDryWell.png\" alt=\"Open Loop Dry Well\">";
-                }elseif($configuration == 2){
-                    echo "\"status-OpenLoop\">";
-                    echo "<img src=\"../../status/image/WebOpenLoop.png\" alt=\"Open Loop\">";
-                }elseif($configuration == 3){
-                    echo "\"status-ClosedLoop\">";
-                    echo "<img src=\"../../status/image/WebClosedLoop.png\" alt=\"Closed Loop\">";
-                }else echo "\"\">";
-                echo "</div>";
-            ?>
-            <?php
-                $query = "SELECT A.SensorLabel,A.SensorName,A.PageLocX,A.PageLocY,B.Recnum,B.SensorColName,B.SysGroup
-                        FROM WebRefTable AS A,SysMap AS B
-                        WHERE A.SensorName = B.SensorRefName AND A.Inhibit = 0 AND B.SysID = 0 AND A.WebSubPageName = 'Main'";
-                $ref = $db -> fetchAll($query);
-                foreach($ref as $result){
-                    //query unique records and if available use that recnum
-                    $query = "SELECT Recnum FROM SysMap WHERE SensorColName = '" . $result['SensorColName'] . "' AND SysGroup = " . $result['SysGroup'] . " AND SysID = " . $systemID;
-                    $unique = $db -> fetchRow($query);
-                    if(isset($unique['Recnum'])) $id = $unique['Recnum'];
-                    else $id = $result['Recnum'];
-                    $xpos = $result['PageLocX'];
-                    $ypos = $result['PageLocY'];
-                    $label = $result['SensorLabel'];
-                    $name = $result['SensorName'];
-                    //$resultZone = $result['WebSubPageName'];
-            ?>
-                    <p class="label-status" style="top:<?=$ypos-20?>px;left:<?=$xpos+5?>px;"><?=$label?></p>
-                    <p class="value-status" style="top:<?=$ypos?>px;left:<?=$xpos?>px;line-height:1.2em">
-                        <a href="javascript:void(0);" class="icon-pencil" title="Edit"
-                            onclick="window.open(
-                                'edit.php?sys=<?=$systemID?>&id=<?=$id?>',
-                                'Popup','width=800,height=350,dependent=yes,0,status=0,resizable=1');">
-                        </a>
-                    </p>
-            <?php
+                if($zone != "Main") echo "<a href=\"./\">Main</a><br>";
+                for($i=1;$i<=$numOfRSM;$i++){
+                    if($zone == "RSM" . $i) continue;
+                    echo "<a href=\"./?z=" . $i . "\">RSM" . $i . "</a><br>";
                 }
             ?>
-            <div class="span10" style="margin-left:50px">
-                <p style="float:right">
-                <?php
-                    if($zone != "Main") echo "<a href=\"./\">Main</a><br>";
-                    for($i=1;$i<=$numOfRSM;$i++){
-                        if($zone == "RSM" . $i) continue;
-                        echo "<a href=\"./?z=" . $i . "\">RSM" . $i . "</a><br>";
-                    }
-                ?>
-                </p>
-            </div>
+            </p>
         </div>
-        <div class="row">
-            <div class="span10 offset1">
-                <br>
-                <button type="submit" class="btn btn-success">
-                    <i class="icon-ok icon-white"></i>
-                    Save
-                </button>
-                <a href="../" class="btn pull-right">
-                    <i class="icon-remove"></i>
-                    Cancel
-                </a>
-            </div>
-        </div>
-    </form>
+    </div>
 </div>
 <?php
     include_once('../../includes/footer.php');
