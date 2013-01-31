48fd006a2f5086077164d1a12a47ef1794f079b5
James Lynch <james.lynch@hvtdc.org>
2013-01-31 13:56:37 -0500

    syntax error, forgot ']'

--- setup/sensor_mapping/index.php
+++ setup/sensor_mapping/index.php
@@ -139,7 +139,7 @@ $sysMapUnique = $db -> fetchAll($query);
             }
         }
         //flag change to check on submit
-        $('[name=sensorMapping' + sourceID + 'onChange').after("<input type=\"hidden\" name=\"Change" + Recnum +"\" value=\"true\">");
+        $('[name=sensorMapping' + sourceID + 'onChange]').after("<input type=\"hidden\" name=\"Change" + Recnum +"\" value=\"true\">");
     }
 </script>
 
