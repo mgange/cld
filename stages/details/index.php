5492677eeb82a252a97c0de202364fc458ebb601
Geoff Young <geoff.young@hvtdc.org>
2013-04-02 16:21:59 -0400

    Add detailed view of system stages for each day

--- stages/details/index.php
+++ stages/details/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *------------------------------------------------------------------------------
- * Stages Index Page
+ * Stages Details
  *------------------------------------------------------------------------------
  *
  */
@@ -35,20 +35,12 @@ function pageName($zone)
     }
 }
 
-require_once('../includes/pageStart.php');
+require_once('../../includes/pageStart.php');
 
 checkSystemSet($config);
 
-if(count($_POST) > 0) {
-    if(isset($_POST['startTime']) && isset($_POST['endTime'])) {
-        $params['start'] = date('Y-m-d', strtotime($_POST['startTime']));
-        $params['end']   = date('Y-m-d', strtotime($_POST['endTime']));
-    }
-    if(isset($_POST['z'])) {
-        $params['z'] = $_POST['z'];
-    }
-
-    header('Location: ./' . buildURLparameters($params));
+if(!isset($_GET['date'])) {
+    header('Location: ../');
 }
 
 $db = new db($config);
@@ -60,15 +52,8 @@ $numRSM = $db -> fetchRow('SELECT NumofRSM FROM SystemConfig WHERE SysID = :SysI
 $numRSM = $numRSM['NumofRSM'];
 
 // Date Stuff
-if(isset($_GET['start']) && isset($_GET['end'])) {
-    $endTime   = $_GET['end'];
-    $startTime = $_GET['start'];
-}else{
-    $endTime = date('Y-m-d');
-    $startTime = date('Y-m-d', strtotime('-1 week'));
-}
-$params['start'] = $startTime;
-$params['end']   = $endTime;
+$d = explode('-', $_GET['date']);
+$theDate = date( mktime(0, 0, 0, $d[1], $d[2], $d[0]) );
 
 // Zone Stuff
 if(isset($_GET['z'])) {
@@ -118,76 +103,68 @@ foreach($sensors as $sensor) {
 }
 
 
-$daysToDisplay = floor((strtotime($endTime) - strtotime($startTime))/(60*60*24));
-
-$maxDays = 180;
-if(! withinRange($daysToDisplay, 1, $maxDays+1)) {
-    $daysToDisplay = $maxDays;
-}
-
-for ($i=0; $i <= $daysToDisplay; $i++) {
 
     $query = "
 SELECT DISTINCT
-    SourceHeader.Recnum,
-    SourceHeader.DateStamp,
-    SourceHeader.TimeStamp,
-    ";
-    foreach($sensors as $sensor){
-        $query .= pickTable($sensor['SourceID']) . '.' . $sensor['SensorColName'] . ",
-    ";
-    }
-    $query .=
-    pickTable($zone).".DigIn01,
-    ".pickTable($zone).".DigIn02,
-    ".pickTable($zone).".DigIn03,
-    ".pickTable($zone).".DigIn04,
-    ".pickTable($zone).".DigIn05
+SourceHeader.Recnum,
+SourceHeader.DateStamp,
+SourceHeader.TimeStamp,
+";
+foreach($sensors as $sensor){
+    $query .= pickTable($sensor['SourceID']) . '.' . $sensor['SensorColName'] . ",
+";
+}
+$query .=
+pickTable($zone).".DigIn01,
+".pickTable($zone).".DigIn02,
+".pickTable($zone).".DigIn03,
+".pickTable($zone).".DigIn04,
+".pickTable($zone).".DigIn05
 FROM
-    SourceHeader";
+SourceHeader";
 foreach($tablesUsed as $table){
-    $query .= ", " . $table;
+$query .= ", " . $table;
 }
 $query .= "
 WHERE SourceHeader.SysID = " . $_SESSION['SysID'];
 foreach($tablesUsed as $table) {
-    $query .= "
-  AND SourceHeader.Recnum = " . $table . ".HeadID";
+$query .= "
+AND SourceHeader.Recnum = " . $table . ".HeadID";
 }
 $query .= "
-  AND SourceHeader.DateStamp = '" . date('Y-m-d', strtotime($endTime."- ".$i." day")) . "'
+AND SourceHeader.DateStamp = '" . date('Y-m-d', $theDate) . "'
 ORDER BY
-    SourceHeader.DateStamp DESC,
-    SourceHeader.TimeStamp DESC
+SourceHeader.DateStamp DESC,
+SourceHeader.TimeStamp DESC
 ";
 
-    $result = $db -> fetchAll($query);
+$result = $db -> fetchAll($query);
 
-    foreach($result as $res) {
-        /* Touch all the system stages so they're created in the correct order */
-        $data[$res['DateStamp']]["System Off"]   += 0;
-        $data[$res['DateStamp']]["Fan Only"]     += 0;
-        $data[$res['DateStamp']]["Emerg. Heat"]  += 0;
-        $data[$res['DateStamp']]["Stage 3 Heat"] += 0;
-        $data[$res['DateStamp']]["Stage 2 Heat"] += 0;
-        $data[$res['DateStamp']]["Stage 1 Heat"] += 0;
-        $data[$res['DateStamp']]["Stage 2 Cool"] += 0;
-        $data[$res['DateStamp']]["Stage 1 Cool"] += 0;
+foreach($result as $res) {
+    /* Touch all the system stages so they're created in the correct order */
+    $data[substr($res['TimeStamp'], 0, 2)]["System Off"]   += 0;
+    $data[substr($res['TimeStamp'], 0, 2)]["Fan Only"]     += 0;
+    $data[substr($res['TimeStamp'], 0, 2)]["Emerg. Heat"]  += 0;
+    $data[substr($res['TimeStamp'], 0, 2)]["Stage 3 Heat"] += 0;
+    $data[substr($res['TimeStamp'], 0, 2)]["Stage 2 Heat"] += 0;
+    $data[substr($res['TimeStamp'], 0, 2)]["Stage 1 Heat"] += 0;
+    $data[substr($res['TimeStamp'], 0, 2)]["Stage 2 Cool"] += 0;
+    $data[substr($res['TimeStamp'], 0, 2)]["Stage 1 Cool"] += 0;
 
-        $stage = Systemlogic(
-            $res['DigIn04'],
-            $res['DigIn01'],
-            $res['DigIn02'],
-            $res['DigIn03'],
-            $res['DigIn05'],
-            0
-        );
-        $data[$res['DateStamp']][$stage]++;
-        $outsideAir[$res['DateStamp']] += $res[$sensors['OutsideAir']['SensorColName']]/100;
-        $datapoints[$res['DateStamp']]++;
-    }
-    // $outsideAir[$result[0]['DateStamp']] = round($outsideAir[$result[0]['DateStamp']]/count($result), 2);
+    $stage = Systemlogic(
+        $res['DigIn04'],
+        $res['DigIn01'],
+        $res['DigIn02'],
+        $res['DigIn03'],
+        $res['DigIn05'],
+        0
+    );
+    $data[substr($res['TimeStamp'], 0, 2)][$stage]++;
+    $outsideAir[substr($res['TimeStamp'], 0, 2)] += $res[$sensors['OutsideAir']['SensorColName']]/100;
+    $datapoints[substr($res['TimeStamp'], 0, 2)]++;
 }
+// $outsideAir[$result[0]['DateStamp']] = round($outsideAir[$result[0]['DateStamp']]/count($result), 2);
+
 foreach($outsideAir as $k => $v) {
     $outsideAir[$k] = round( $v / $datapoints[$k] , 2);
 }
@@ -195,7 +172,7 @@ foreach($outsideAir as $k => $v) {
 $data = array_reverse($data);
 $outsideAir = array_reverse($outsideAir);
 
-require_once('../includes/header.php');
+require_once('../../includes/header.php');
 ?>
         <script>
         var chartType = 'column';
@@ -210,17 +187,6 @@ require_once('../includes/header.php');
                 borderWidth: 1,
                 shadow: 0,
                 stacking: 'percent',
-                point: {
-                    events: {
-                        click: function(){
-                            d = new Date(categories[this.x]);
-                            window.location = './details?date='
-                                + d.getFullYear()+'-'
-                                +(d.getMonth()+1)+'-'
-                                + d.getDate();
-                        }
-                    }
-                }
             }
         };
         var tooltip = {
@@ -260,13 +226,20 @@ require_once('../includes/header.php');
             }
         ];
         var categories = [<?php
-            $i = 1;
-            foreach($data as $date => $vals) {
-                echo "'" . date('M d, Y', strtotime($date)) . "'";
-                if($i < count($data)) {
-                    echo ", ";
+            $half = 'AM';
+            for ($i=0; $i < 24; $i++) {
+                echo "'";
+                if($i == 0) {
+                    echo 12;
+                }else{
+                    if($i > 12) {
+                        echo $i - 12;
+                    }else{
+                        echo $i;
+                    }
                 }
-                $i++;
+                echo " " . $half . "'";
+                if($i < 23) { echo ', ';}
             }
         ?>];
         var data = [
@@ -380,10 +353,7 @@ if($numRSM > 0) {
             <div class="rsmToggle btn-group span3">
                 <a
                     class="btn btn-mini <?php if(!isset($_GET['z']) || $_GET['z'] == 0){echo ' active';} ?>"
-                    href="./<?php
-                        $params['z'] = 0;
-                        echo buildURLparameters($params);
-                    ?>">
+                    href="./?date=<?php echo date('Y-m-d', $theDate); ?>">
                     Main
                 </a>
 <?php
@@ -391,10 +361,7 @@ if($numRSM > 0) {
 ?>
                 <a
                     class="btn btn-mini<?php if($_GET['z']==$i){echo ' active';} ?>"
-                    href="./<?php
-                        $params['z'] = $i;
-                        echo buildURLparameters($params);
-                    ?>">
+                    href="./?date=<?php echo date('Y-m-d', $theDate); ?>&z=<?php echo $i; ?>">
                     RSM<?php
                     if($numRSM > 1){echo '-'.$i;}
                     ?>
@@ -418,34 +385,7 @@ if(isset($_GET['z'])) {
                 </div>
             </div>
 
-            <div class="row">
-                <div class="span6 offset3">
-                    <h5 class="align-center">Display Data</h5>
-                    <form class="form-inline" action="./" method="POST">
-                        <div class="row">
-                            <label class="span3" for="startTime">From
-                                <input
-                                    id="startTime"
-                                    class="datepick span3"
-                                    type="text" name="startTime"
-                                    value="<?php echo $startTime; ?>">
-                            </label>
-                            <label class="span3" for="endTime">Until
-                                <input
-                                    id="endTime"
-                                    class="datepick span3"
-                                    type="text"
-                                    name="endTime"
-                                    value="<?php echo $endTime; ?>">
-                            </label>
-                        </div>
-                        <br>
-                        <input class="btn btn-info btn-large btn-block" type="submit" value="Submit">
-                        <input type="hidden" name="z" value="<?php echo $params['z']; ?>">
-                    </form>
-                </div>
-            </div>
 
 <?php
-require_once('../includes/footer.php');
+require_once('../../includes/footer.php');
 ?>
